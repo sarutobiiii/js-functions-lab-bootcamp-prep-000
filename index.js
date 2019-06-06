6d00b520d406c03ab32836081af75ec48e9b50b2
oluwatobi balogun <tobibags19@gmail.com>
2019-06-06 00:49:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return console.log("Happy holidays, ${name}!")
+  return console.log(`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo(holiday, string) {
   return console.log(`Happy ${holiday}, ${name}!`)
-  
+}
+
+function 
 
